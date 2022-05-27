9a7dc538dbe831a31e8f31871a66f006b144c883
Henrique <henriqueleone.dev@gmail.com>
2022-05-26 21:46:33 -0300

    final commit

---
+++ script.js
@@ -0,0 +1,72 @@
+var vermelho = document.getElementById('btn-red')
+var azul = document.getElementById('btn-blue')
+var amarelo = document.getElementById('btn-yellow')
+var laranja = document.getElementById('btn-orange')
+var verde = document.getElementById('btn-green')
+var corpo = document.getElementById('body')
+
+function changeColorRed(){
+    if (corpo.classList.contains('red')){
+        corpo.classList.remove('red');
+    } else {
+        corpo.classList.add('red');
+        corpo.style.transitionDuration='.5s'
+        corpo.classList.add('yellow');
+        corpo.classList.remove('green');
+        corpo.classList.remove('orange');
+        corpo.classList.remove('yellow');
+        corpo.classList.remove('blue');
+    }
+}
+
+function changeColorBlue(){
+    if (corpo.classList.contains('blue')){
+        corpo.classList.remove('blue');
+    } else {
+        corpo.style.transitionDuration='.5s'
+        corpo.classList.add('blue');
+        corpo.classList.remove('green');
+        corpo.classList.remove('orange');
+        corpo.classList.remove('yellow');
+        corpo.classList.remove('red');
+    }
+}
+
+function changeColorYellow(){
+    if (corpo.classList.contains('yellow')){
+        corpo.classList.remove('yellow');
+    } else {
+        corpo.style.transitionDuration='.5s'
+        corpo.classList.add('yellow');
+        corpo.classList.remove('green');
+        corpo.classList.remove('orange');
+        corpo.classList.remove('blue');
+        corpo.classList.remove('red');
+    }
+}
+
+function changeColorOrange(){
+    if (corpo.classList.contains('orange')){
+        corpo.classList.remove('orange');
+    } else {
+        corpo.style.transitionDuration='.5s'
+        corpo.classList.add('orange');
+        corpo.classList.remove('green');
+        corpo.classList.remove('yellow');
+        corpo.classList.remove('blue');
+        corpo.classList.remove('red');
+    }
+}
+
+function changeColorGreen(){
+    if (corpo.classList.contains('green')){
+        corpo.classList.remove('green');
+    } else {
+        corpo.style.transitionDuration='.5s'
+        corpo.classList.add('green');
+        corpo.classList.remove('orange');
+        corpo.classList.remove('yellow');
+        corpo.classList.remove('blue');
+        corpo.classList.remove('red');
+    }
+}
